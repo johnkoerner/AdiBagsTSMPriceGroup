dd6bad572c918532ce59d9e141b2f0d2e70a7cbf
John Koerner <johnkoern@gmail.com>
2022-11-21 21:16:33 -0500

    Updated for Dragonflight

--- scripts/deploy.ps1
+++ scripts/deploy.ps1
@@ -1,3 +1,4 @@
-$dest = "F:\\World of Warcraft\\_retail_\\Interface\\AddOns\\AdiBags_TSM_Price_Groups"
+#$dest = "F:\\World of Warcraft\\_retail_\\Interface\\AddOns\\AdiBags_TSM_Price_Groups"
+$dest = "C:\\Program Files (x86)\\World of Warcraft\\_retail_\\Interface\\AddOns\\AdiBags_TSM_Price_Groups"
 $src = ".\AdiBags_TSM_Price_Groups\*"
 Copy-Item -Path "$src" -Destination "$dest" -Recurse -Force -Verbose
